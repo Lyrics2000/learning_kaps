026a94427bfae21a9789980599ab73ce91d39e92
Lyrics2000 <thomasambetsa@gmail.com>
2023-09-28 14:38:58 +0300

    arithmetics

---
+++ arithmetica&logiccal/app.py
@@ -0,0 +1,33 @@
+# arithmeetics operations 
+
+## additions. 
+# int
+
+age = 20 
+wieght = 10 
+
+human = age + wieght 
+
+print("int add ", human)
+
+
+# float 
+age1 = 20.1 
+wieght1 = 10.2
+
+human1 = age1 + wieght1
+
+print("float add ",human1)
+
+
+# string
+age1 = "20.1"
+wieght1 = "10.2"
+
+human1 = age1 + wieght1
+
+print("float add ",human1)
+
+
+
+
