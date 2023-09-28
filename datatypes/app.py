af56fed0fc474326c1ba37abefaf2f8fef763df5
Lyrics2000 <thomasambetsa@gmail.com>
2023-09-28 14:30:51 +0300

    leearned datatypes

---
+++ datatypes/app.py
@@ -0,0 +1,39 @@
+# Online Python compiler (interpreter) to run Python online.
+## string
+import datetime
+
+name = "john"
+Name = "john"
+
+name = 'john'
+name = """john """
+name = str('name')
+
+# integer 
+
+age = 20
+age = int(20)
+
+# float 
+
+weight = 20.1
+weight1 = float(20.1)
+
+print(weight,weight1)
+
+today_date = datetime.datetime.now()
+
+print("today is", today_date)
+
+
+# convert int to string 
+
+age1 = "20"
+
+# ariythmetic 
+
+ans = int(age1)
+
+print(ans)
+ 
+ 
