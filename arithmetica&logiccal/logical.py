6fe665bcaca08a12240d2a2409812ce436031e0b
Lyrics2000 <thomasambetsa@gmail.com>
2023-09-28 14:55:02 +0300

    added logic

---
+++ arithmetica&logiccal/logical.py
@@ -0,0 +1,23 @@
+# logicakl operatiobns 
+
+# less than 
+
+age  = 20 
+age1 =  30 
+
+print(age < age1) # less than
+print(age > age1) # greator than
+print(age == age1) # equial to
+
+# and 
+
+# True  and True = True 
+# True and False = False
+# False and False = False
+# True or True =. True 
+# True or False. =. True
+# False or False. =. False
+
+
+print(age < age1 and age > age1)
+print(age < age1 or age > age1)
